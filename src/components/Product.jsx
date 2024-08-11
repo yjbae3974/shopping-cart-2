e8ef7ab6e38226ce58605f7349134b7bf7876230
정석민 <jyeong1212@naver.com>
2024-08-12 01:30:23 +0900

    feat: Product 컴포넌트 적용

---
+++ src/components/Product.jsx
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled";
+import { Button } from "./Button";
+import { useNavigate } from "react-router-dom";
+import { PAGE } from "constants/common";
+import { Box } from "styles/StyleComponent";
+
+export const Product = ({ product, cart, setCart, ...rest }) => {
+  const navigate = useNavigate();
+
+  const handleCart = (product) => {
+    if (cart.find((item) => item.id === product.id)) {
+      alert("이미 장바구니에 추가된 상품입니다.");
+      return;
+    }
+    setCart((prev) => [...prev, product]);
+    alert("장바구니에 추가되었습니다.");
+  };
+
+  return (
+    <Item {...rest}>
+      <Box gap={6}>
+        <Name
+          dangerouslySetInnerHTML={{
+            __html: product.name.replace(/\\n/g, "<br/>"),
+          }}
+        />
+        <Description>{product.description}</Description>
+      </Box>
+      <Box gap={4} style={{ width: "fit-content" }}>
+        <Button onClick={() => navigate(`${PAGE.PRODUCT}/${product.id}`)}>
+          제품 설명 보기
+        </Button>
+        <Button onClick={() => handleCart(product)}>장바구니 담기</Button>
+      </Box>
+    </Item>
+  );
+};
+
+const Item = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 20px;
+  scroll-snap-align: start;
+  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.12);
+  border-radius: 6px;
+  overflow: hidden;
+  padding: 16px;
+  cursor: pointer;
+`;
+const Name = styled.div`
+  font-family: "Pretendard Variable", sans-serif;
+  font-size: 20px;
+  font-weight: 550;
+  line-height: 135%;
+  color: black;
+  text-align: start;
+  word-break: break-all;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  background: linear-gradient(to bottom, pink 70%, transparent 60%);
+  background-size: 100% 40%;
+  background-repeat: no-repeat;
+  background-position: 0 100%; /* Bottom alignment */
+`;
+const Description = styled.div`
+  font-family: "Pretendard Variable", sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 160%;
+  color: gray;
+  text-align: start;
+  word-break: break-all;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,42 +1,23 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Button } from "components/Button";
 import { GNB } from "components/GNB";
-import { GNB_TYPE, PAGE } from "constants/common";
-import { useNavigate } from "react-router-dom";
+import { GNB_TYPE } from "constants/common";
+import { ProductInCart } from "components/ProductInCart";
+import { Box } from "styles/StyleComponent";
 
 function CartPage({ cart, setCart }) {
-  const navigate = useNavigate();
-
-  const handleRemove = (consulting) => {
-    const newCart = cart.filter((item) => item !== consulting);
-    setCart(newCart);
-  };
-
   return (
     <Base>
       <GNB type={GNB_TYPE.MAIN} />
       <Inner>
         <Box gap={30}>
           {cart.map((consulting, id) => (
-            <Item key={id}>
-              <Box gap={6}>
-                <Name
-                  dangerouslySetInnerHTML={{
-                    __html: consulting.name.replace(/\\n/g, "<br/>"),
-                  }}
-                />
-                <Description>{consulting.description}</Description>
-              </Box>
-              <Box gap={4} style={{ width: "fit-content" }}>
-                <Button onClick={() => navigate(`${PAGE.PRODUCT}/${id}`)}>
-                  제품 설명 보기
-                </Button>
-                <Button onClick={() => handleRemove(consulting)}>
-                  제거하기
-                </Button>
-              </Box>
-            </Item>
+            <ProductInCart
+              key={id}
+              product={consulting}
+              cart={cart}
+              setCart={setCart}
+            />
           ))}
         </Box>
       </Inner>
@@ -55,59 +36,4 @@ const Inner = styled.div`
   flex-direction: column;
   gap: 20px;
   padding: 72px 20px 69px;
-`;
-const Item = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  gap: 20px;
-  scroll-snap-align: start;
-  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.12);
-  border-radius: 6px;
-  overflow: hidden;
-  padding: 16px;
-  cursor: pointer;
-`;
-export const Box = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: ${({ direction }) => direction || "column"};
-  justify-content: ${({ content }) => content || "flex-start"};
-  align-items: ${({ align }) => align || "flex-start"};
-  gap: ${({ gap }) => gap || 0}px;
-  background-color: ${({ bgColor }) => bgColor || "transparent"};
-`;
-const Name = styled.div`
-  font-family: "Pretendard Variable", sans-serif;
-  font-size: 20px;
-  font-weight: 550;
-  line-height: 135%;
-  color: black;
-  text-align: start;
-  word-break: break-all;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  background: linear-gradient(to bottom, pink 70%, transparent 60%);
-  background-size: 100% 40%;
-  background-repeat: no-repeat;
-  background-position: 0 100%; /* Bottom alignment */
-`;
-const Description = styled.div`
-  font-family: "Pretendard Variable", sans-serif;
-  font-size: 15px;
-  font-weight: 500;
-  line-height: 160%;
-  color: gray;
-  text-align: start;
-  word-break: break-all;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
 `;
