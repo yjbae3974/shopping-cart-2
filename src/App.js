44fc05dd7f5d03cec3b1c5a22d32cc2f17000200
정석민 <jyeong1212@naver.com>
2024-08-11 17:59:11 +0900

    feat: 장바구니 목록 로컬 스토리지에 저장

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    // 로컬 스토리지에서 장바구니 데이터를 가져옴
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(() => {
+    // 장바구니 데이터가 변경될 때마다 로컬 스토리지에 저장
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <Router>
