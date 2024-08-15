fb31b0106601d4a1785214ea75b090390624c1da
정석민 <jyeong1212@naver.com>
2024-08-15 16:55:39 +0900

    style: 장바구니 목록 없을 때, 스타일 추가

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -11,14 +11,18 @@ function CartPage({ cart, setCart }) {
       <GNB type={GNB_TYPE.MAIN} />
       <Inner>
         <Box gap={30}>
-          {cart.map((product, id) => (
-            <ProductInCart
-              key={id}
-              product={product}
-              cart={cart}
-              setCart={setCart}
-            />
-          ))}
+          {!cart || cart.length <= 0 ? (
+            <Text>등록된 상품이 없습니다.</Text>
+          ) : (
+            cart.map((product, id) => (
+              <ProductInCart
+                key={id}
+                product={product}
+                cart={cart}
+                setCart={setCart}
+              />
+            ))
+          )}
         </Box>
       </Inner>
     </Base>
@@ -36,4 +40,15 @@ const Inner = styled.div`
   flex-direction: column;
   gap: 20px;
   padding: 72px 20px 69px;
+`;
+const Text = styled.div`
+  font-family: "Pretendard Variable", sans-serif;
+  font-size: 20px;
+  font-weight: 550;
+  line-height: 135%;
+  text-align: center;
+  color: #717171;
+
+  width: 100%;
+  margin-top: 60px;
 `;
